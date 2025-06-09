a75dfe75f13a819d5c3055771d392a24e56c55ab
hutao <9675520@qq.com>
2025-06-09 20:20:46 +0800

    Add files via upload

---
+++ nezha.js
@@ -0,0 +1,40 @@
+<script>
+  window.CustomBackgroundImage = 'https://img.picui.cn/free/2025/04/13/67fa922e78f65.jpg'
+  window.CustomMobileBackgroundImage = 'https://img.picui.cn/free/2025/04/13/67fa922e78f65.jpg'
+  window.CustomLogo = 'https://img.picui.cn/free/2025/04/16/67fe9378eb036.png'
+  window.CustomDesc = '服务器监控'
+  window.ShowNetTransfer = true
+  window.DisableAnimatedMan = false
+  window.CustomIllustration = 'https://img.picui.cn/free/2025/04/13/67fa922f20f37.gif'
+  window.FixedTopServerName = true
+ window.CustomLinks =
+   '[{"link":"https://sms.857999.xyz/","name":"联系我"}]'
+  window.ForceTheme = 'dark'
+</script>
+
+<script src="https://cdn.jsdelivr.net/gh/mocchen/cssmeihua/js/aixin.js"></script>/* 点击爱心特效 */
+<script src="https://cdn.jsdelivr.net/gh/mocchen/cssmeihua/js/yinghua.js"></script>/* 页面樱花效果 */
+
+<span class="js-cursor-container"></span>
+
+<script src="https://cdn.jsdelivr.net/gh/mocchen/cssmeihua/js/xiaoxingxing.js"></script>
+/* 以上鼠标特效 */
+
+<style> 
+  .dark .bg-card {
+   font-family: "Roboto", "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: #ffffff; /* 白色，高对比度 */
+    position: relative; /* 设置元素为相对定位 */
+    background-color: rgba(0, 0, 0, 0.3); /* 设置背景颜色为半透明的黑色 */
+    backdrop-filter: blur(5px); /* 应用毛玻璃效果，模糊度为5像素 */
+    border-radius: 15px; /* 设置边框圆角为15像素 */
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
+    //overflow: hidden;  /* 确保内容不会溢出容器 */
+    }
+  .dark .bg-card h1, .dark .bg-card h2, .dark .bg-card h3 {
+    color: #ffffff; /* 保持标题的高对比度 */
+  }
+  .dark .bg-card p {
+    color: #eeeeee; /* 段落文本稍微柔和一些 */
+  }
+</style>
