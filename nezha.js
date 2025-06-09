5663404584493ace6802d4924d5ede84370bb41f
hutao <9675520@qq.com>
2025-06-09 20:26:59 +0800

    Update nezha.js

--- nezha.js
+++ nezha.js
@@ -38,3 +38,15 @@
     color: #eeeeee; /* 段落文本稍微柔和一些 */
   }
 </style>
+/* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 *//* 周期性流量进度条 */
+<script>
+  window.TrafficScriptConfig = {
+    showTrafficStats: true,    // 显示流量统计, 默认开启
+    insertAfter: true,         // 如果开启总流量卡片, 是否放置在总流量卡片后面, 默认为true
+    interval: 60000,           // 60秒刷新缓存, 单位毫秒, 默认60秒
+    toggleInterval: 5000,      // 4秒切换流量进度条右上角内容, 0秒不切换, 单位毫秒, 默认5秒
+    duration: 500,             // 缓出缓进切换时间, 单位毫秒, 默认500毫秒
+    enableLog: false           // 开启日志, 默认关闭
+  };
+</script>
+<script src="https://raw.githubusercontent.com/hutaoee/nezha.peizhi/refs/heads/main/liuliang.js"></script>
